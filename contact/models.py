e5716e46fbf3862e274c02dbaca446a3947064d2
proguimador1 <guilherme.programmer1@gmail.com>
2025-03-27 10:13:15 -0300

    Added verbose name to model category

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
     name = models.CharField(max_length=20)
 
     def __str__(self):
