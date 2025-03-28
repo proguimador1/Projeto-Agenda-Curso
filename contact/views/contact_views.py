8352573d7ded8d4bd354bb04ed5c45c0b3eb91e1
proguimador1 <guilherme.programmer1@gmail.com>
2025-03-28 10:36:57 -0300

    Created the contact list page

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 def home(request):
+    contacts = Contact.objects.all()
     return render(
-        request,
-        'contact/index.html',
+            request,
+            'contact/index.html',
+            {
+                'contacts': contacts
+            }
         )
