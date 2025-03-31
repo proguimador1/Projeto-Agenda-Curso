cd180642547c77f00c89625bc93fe03d35919727
proguimador1 <guilherme.programmer1@gmail.com>
2025-03-31 09:27:43 -0300

    Added search function

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<int:contact_id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
     path('', views.home, name='home'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 def home(request):
@@ -18,5 +19,27 @@ def contact(request, contact_id):
             'contact/contact.html',
             {
                 'contact': single_contact
+            }
+        )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('home')
+
+    contacts = Contact.objects.filter(show=True)\
+    .filter(
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value) 
+    )\
+    .order_by('-id')
+    return render(
+            request,
+            'contact/index.html',
+            {
+                'contacts': contacts
             }
         )
