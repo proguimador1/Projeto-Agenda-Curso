faddf42411a25d64fb7dc4ada0ffc9575f69871d
proguimador1 <guilherme.programmer1@gmail.com>
2025-04-01 10:50:15 -0300

    Made some ajustments to the form widgets

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,26 @@ from django.core.exceptions import ValidationError
 from contact.models import Contact
 
 class ContactForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['first_name'].widget.attrs.update({
+            'placeholder': 'Ex: Joaquim'
+        })
+        self.fields['last_name'].widget.attrs.update({
+            'placeholder': 'Ex: da Silva'
+        })
+        self.fields['phone'].widget.attrs.update({
+            'placeholder': 'Ex: (00)00000-0000'
+        })
+        self.fields['email'].widget.attrs.update({
+            'placeholder': 'Ex: fulanotal@email.com'
+        })
+        self.fields['description'].widget.attrs.update({
+            'placeholder': 'A short description of the contact',
+            'style': 'max-height: 10rem;max-width: 80rem;resize: none'
+        })
+
     class Meta:
         model = Contact
         fields = 'first_name', 'last_name', 'phone', 'email',\
