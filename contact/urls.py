4f309bf130f7e7e14bee3e551a6728c465ad5f60
proguimador1 <guilherme.programmer1@gmail.com>
2025-04-02 11:24:56 -0300

    Created the contact update page

--- contact/urls.py
+++ contact/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
-    #path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
     #path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from contact.forms import ContactForm
+from contact.models import Contact
 
 def create(request):
+    form_action = reverse('create')
+
     if request.method == 'POST':
         form = ContactForm(data=request.POST)
 
@@ -14,6 +18,7 @@ def create(request):
             'contact/create.html',
             {
                 'form': form,
+                'form_action': form_action,
                 'site_title': 'Create contact',
             }
         )
@@ -23,6 +28,38 @@ def create(request):
             'contact/create.html',
             {
                 'form': ContactForm(),
+                'form_action': form_action,
+                'site_title': 'Create contact',
+            }
+        )
+
+def update(request, contact_id):
+    form_action = reverse('update', args=(contact_id,))
+    contact = get_object_or_404(Contact, id=contact_id)
+
+    if request.method == 'POST':
+        form = ContactForm(data=request.POST, instance=contact)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+        return render(
+            request,
+            'contact/create.html',
+            {
+                'form': form,
+                'form_action': form_action,
+                'site_title': 'Create contact',
+            }
+        )
+
+    return render(
+            request,
+            'contact/create.html',
+            {
+                'form': ContactForm(instance=contact),
+                'form_action': form_action,
                 'site_title': 'Create contact',
             }
         )
