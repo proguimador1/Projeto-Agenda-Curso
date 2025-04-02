ca8a8fb1bf245c1ab767dc6cc5088170cd809310
proguimador1 <guilherme.programmer1@gmail.com>
2025-04-02 10:21:05 -0300

    Added functions to save the contact and redirect to homepage

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import ContactForm
 
 def create(request):
     if request.method == 'POST':
+        form = ContactForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
         return render(
             request,
             'contact/create.html',
             {
-                'form': ContactForm(data=request.POST),
+                'form': form,
                 'site_title': 'Create contact',
             }
         )
