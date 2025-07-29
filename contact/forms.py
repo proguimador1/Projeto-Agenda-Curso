8c968f897ae629bc560da491db43c75a197d8d63
Guilherme Andrade <guilherme.programmer1@gmail.com>
2025-07-29 08:53:27 -0300

    Added new fields to user forms, and made validation methods

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from contact.models import Contact
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 def number_in_name_validation(name):
     if any(c.isdigit() for c in name):
@@ -88,4 +89,23 @@ class ContactForm(forms.ModelForm):
         return phone
     
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    username = forms.CharField(required=True)
+    
+    class Meta:
+        model = User
+        fields = 'first_name', 'last_name', 'email',\
+        'username',
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('This email is alredy used', code='invalid'),
+            )
+
+        return email
