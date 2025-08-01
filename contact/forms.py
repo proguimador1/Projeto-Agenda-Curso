48d27aae84a55bf2dd463a365e52277d521fa69b
Guilherme Andrade <guilherme.programmer1@gmail.com>
2025-08-01 17:19:03 -0300

    Hid update and delete buttons from non-owners of the contact

--- contact/forms.py
+++ contact/forms.py
@@ -39,7 +39,8 @@ class ContactForm(forms.ModelForm):
                attrs={
                   'accept': 'image/*',
              }
-            )
+            ),
+            required=False
         )
 
     class Meta:
